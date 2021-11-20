aea2c156140ae4309e9d54a7796f096c7e85b871
QWERTY <77407536+stechwer@users.noreply.github.com>
2021-11-20 18:56:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(function(){
+    var Mixer = mixitup ('.kitchen-wrapper');
+});
---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('.header-burger').on('click',function(){
+        $('.header-nav').toggleClass('active');
+    });
+});
